c4502cef3c42199d04659f6304561bfc058b776f
RPJ <ropas5@alumni.uv.es>
2026-02-27 13:05:38 +0100

    Creamos modelo de CNN, con automatización de la visualización de las metrias

---
+++ notebooks/models/simple_cnn.py
@@ -0,0 +1,19 @@
+# models/simple_cnn.py
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.optimizers import Adam
+
+def create_model(input_shape=(128,128,3), num_classes=4):
+    model = Sequential()
+    # Capa convolucional simple
+    model.add(Conv2D(16, kernel_size=3, activation='relu', input_shape=input_shape))
+    
+    # Flatten y capa de salida
+    model.add(Flatten())
+    model.add(Dense(num_classes, activation='softmax'))
+
+    # Compilación
+    optimizer = Adam()
+    model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+
+    return model
