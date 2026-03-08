e82b0e8594dd9961b615ac0b8273823bd3625389
RPJ <ropas5@alumni.uv.es>
2026-03-08 20:58:45 +0100

    mejoramos el modelo

--- models/complejo_cnn.py
+++ models/complejo_cnn.py
@@ -7,32 +7,36 @@ from tensorflow.keras.regularizers import l1_l2, l2
 
 def create_model(input_shape=(128,128,3), num_classes=4, l_rate=0.01):
     model = Sequential()
-    # Capa 1
-    model.add(Conv2D(12, kernel_size=7, activation='relu', input_shape=input_shape))
+    # Bloque 1
+    model.add(Conv2D(32, kernel_size=7, activation='relu', input_shape=input_shape))
     model.add(BatchNormalization())
     model.add(Dropout(0.3))
-    model.add(MaxPooling2D())
+    model.add(MaxPooling2D(pool_size=2))
     
-    # Capa 2
-    model.add(Conv2D(12, kernel_size=3, activation='relu'))
+    # Bloque 2
+    model.add(Conv2D(64, kernel_size=5, activation='relu'))
     model.add(BatchNormalization())
     model.add(Dropout(0.3))
-    model.add(MaxPooling2D())
+    model.add(MaxPooling2D(pool_size=2))
 
-    # Capa 3
+    # Bloque 3
+    model.add(Conv2D(128, kernel_size=3, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.3))
+    model.add(MaxPooling2D(pool_size=2))
+
+    # Global pooling
     model.add(GlobalAveragePooling2D())
 
-    # Capa 4  Full conect
+    # Capas densas
     model.add(Dense(1024, kernel_regularizer= l1_l2(0.0001,0.0001), activation= "relu"))
     model.add(BatchNormalization())
     model.add(Dropout(0.4))
 
-    # Capa 5
-    model.add(Dense(512, kernel_regularizer= l1_l2(0.0001,0.0001), activation= "relu"))
+    model.add(Dense(512, kernel_regularizer= l1_l2(0.00005,0.00005), activation= "relu"))
     model.add(BatchNormalization())
     model.add(Dropout(0.4))
 
-    # Capa 6
     model.add(Dense(256, kernel_regularizer= l1_l2(0.0001,0.0001), activation= "relu"))
     model.add(BatchNormalization())
     model.add(Dropout(0.4))
